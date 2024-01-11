d88032735749e59ae621cf529409ad5acd44d93b
badmen007 <1572226525@qq.com>
2024-01-11 23:27:59 +0800

    test

--- blog.sh
+++ blog.sh
@@ -13,7 +13,7 @@ npm run build
 echo "开始构建镜像"
 docker build -t blog:1.0 .
 echo '删除旧容器'
-docker stop blog-container
-docker rm blog-container
+docker stop blog
+docker rm blog
 echo '启动新容器'
 docker container run -p 80:80 -d --name blog-container blog:1.0
